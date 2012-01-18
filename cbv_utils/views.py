d2bbedf1b78033a200dbc0244c71a8f7b772e603
Travis Swicegood <development@domain51.com>
2012-01-18 13:09:38 -0600

    add missing import

--- cbv_utils/views.py
+++ cbv_utils/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.views.generic.edit import FormMixin as DjangoFormMixin
 from django.views.generic.edit import ProcessFormView as DjangoProcessFormView
 
